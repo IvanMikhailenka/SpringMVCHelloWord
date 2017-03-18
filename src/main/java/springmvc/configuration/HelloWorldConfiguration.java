1e85dd58ec195e7daa19a87bc77f080d2055164c
ivan <ivanmikhailenka@gmail.com>
2017-03-19 00:40:14 +0300

    StringMVC hello word example

---
+++ src/main/java/springmvc/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,30 @@
+package springmvc.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "springmvc")
+public class HelloWorldConfiguration extends WebMvcConfigurerAdapter {
+
+	@Bean(name="HelloWorld")
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+}
